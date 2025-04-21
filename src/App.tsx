af3618516374f95a6794d845b759536d38ff2b01
theghost1980 <saturnob612@gmail.com>
2025-04-21 14:17:33 -0400

    initial commit: basic webpck/react/ts/chakraUI boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+const App = () => (
+  <Box p={8}>
+    <Heading mb={4}>¡Hola, Chakra UI!</Heading>
+    <Text>
+      Este es un ejemplo básico usando Chakra UI con React y TypeScript.
+    </Text>
+  </Box>
+);
+
+export default App;
